b9f620687978e1e85a62afc018308069afd228dc
Ryu-134 <147681738+Ryu-134@users.noreply.github.com>
2023-11-14 20:04:13 -0800

    V2.0 - Phase 2 - CSV File Produced for a category

--- main.py
+++ main.py
@@ -2,48 +2,65 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-page = requests.get(url)
-print(page.content)
 
-soup = BeautifulSoup(page.content, "html.parser")
-title_td = soup.find("h1")
-upc_td = soup.find("th", string="UPC").find_next_sibling("td")
-price_incl_tax_td = soup.find("th", string="Price (incl. tax)").find_next_sibling("td")
-price_excl_tax_td = soup.find("th", string="Price (excl. tax)").find_next_sibling("td")
-quantity_available_td = soup.find("th", string="Availability").find_next_sibling("td")
-product_description_td = soup.find("div", id="product_description").find_next_sibling("p")
-category_td = soup.find("th", string="Product Type").find_next_sibling("td")
-rating_td = soup.find("th", string="Number of reviews").find_next_sibling("td")
-image_url_td = soup.find("img").get("src")
+# Initialize Arrays to load data into
+(urls, upcs, titles, price_incl_taxs, price_excl_taxs, quantity_availables, product_descriptions,
+ categories, ratings, image_urls) = ([], [], [], [], [], [], [], [], [], [])
 
-title = title_td.text
-upc = upc_td.text
-price_incl_tax = price_incl_tax_td.text
-price_excl_tax = price_excl_tax_td.text
-quantity_available = quantity_available_td.text
-product_description = product_description_td.text
-category = category_td.text
-rating = rating_td.text
+# Function to get data from URL to scrape and append to lists
+def scrape_book_data(book_url):
+    response = requests.get(book_url)
+    book_page_soup = BeautifulSoup(response.content, "html.parser")
 
-print(f"The URL is: {url}")
-print(f"The UPC is: {upc}")
-print(f"The title is: {title}")
-print(f"The price (incl. tax) is: {price_incl_tax}")
-print(f"The price (excl. tax) is: {price_excl_tax}")
-print(f"Product availability: {quantity_available}")
-print(f"Description: {product_description}")
-print(f"Category: {category}")
-print(f"Number of Reviews: {rating}")
-print(f"Image URL: {image_url_td}")
+    # Locate all necessary data and extract
+    title_td = book_page_soup.find("h1").string
+    upc_td = book_page_soup.find("th", string="UPC").find_next_sibling("td").string
+    price_incl_tax_td = book_page_soup.find("th", string="Price (incl. tax)").find_next_sibling("td").string
+    price_excl_tax_td = book_page_soup.find("th", string="Price (excl. tax)").find_next_sibling("td").string
+    quantity_available_td = book_page_soup.find("th", string="Availability").find_next_sibling("td").string
+    product_description_td = book_page_soup.find("div", id="product_description").find_next_sibling("p").string
+    category_td = book_page_soup.find("th", string="Product Type").find_next_sibling("td").string
+    rating_td = book_page_soup.find("th", string="Number of reviews").find_next_sibling("td").string
+    image_url_td = book_page_soup.find("img")["src"]
 
-headers = ["url", "UPC", "title", "price_incl_tax", "price_excl_tax",
+    # Append data to lists
+    urls.append(book_url)
+    upcs.append(upc_td)
+    titles.append(title_td)
+    price_incl_taxs.append(price_incl_tax_td)
+    price_excl_taxs.append(price_excl_tax_td)
+    quantity_availables.append(quantity_available_td)
+    product_descriptions.append(product_description_td)
+    categories.append(category_td)
+    ratings.append(rating_td)
+    image_urls.append(image_url_td)
+
+# Scrape main url page by going into other urls
+base_url = "https://books.toscrape.com/"
+main_url = f"{base_url}catalogue/category/books/poetry_23/index.html"
+main_page = requests.get(main_url)
+main_soup = BeautifulSoup(main_page.content, "html.parser")
+book_links = main_soup.select("h3 a")
+
+# Correct relative URL to properly scrape the book and print
+for link in book_links:
+    book_url = f"{base_url}{link["href"].replace("../../..", "catalogue") }"
+    scrape_book_data(book_url)
+    print("test")
+# Test to see if list is populated
+print(urls)
+
+# CSV headers
+headers = ["URL", "UPC", "title", "price_incl_tax", "price_excl_tax",
            "quantity_available", "product_description", "category", "rating", "image_url_td"]
 
-with open("data.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
+# Write to CSV
+with open("books_data.csv", "w", newline="", encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile)
     writer.writerow(headers)
-    for i in range(len(url)):
-        row = [url[i], upc[i], title[i], price_incl_tax[i], price_excl_tax[i],
-           quantity_available[i], product_description[i], category[i], rating[i], image_url_td[i]]
+
+# Write each book data as row in CSV-
+    for i in range(len(urls)):
+        row = [urls[i], upcs[i], titles[i], price_incl_taxs[i], price_excl_taxs[i],
+           quantity_availables[i], product_descriptions[i], categories[i], ratings[i], image_urls[i]]
         writer.writerow(row)
