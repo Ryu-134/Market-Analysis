03b8a4ec3088d21d41320a0f7e7e698a4aab483c
Ryu-134 <147681738+Ryu-134@users.noreply.github.com>
2023-11-16 18:24:29 -0800

    V3.0 - Phase 3 - Incomplete CSV File for all categories since only 1/2 of the books are in the CSV File

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def scrape_book_data(book_url):
     price_incl_tax_td = book_page_soup.find("th", string="Price (incl. tax)").find_next_sibling("td").string
     price_excl_tax_td = book_page_soup.find("th", string="Price (excl. tax)").find_next_sibling("td").string
     quantity_available_td = book_page_soup.find("th", string="Availability").find_next_sibling("td").string
-    product_description_td = book_page_soup.find("div", id="product_description").find_next_sibling("p").string
+    product_description_td = book_page_soup.find("div", id="product_description").find_next_sibling("p").string \
+        if book_page_soup.find('div', id='product_description') else 'No description'
     category_td = book_page_soup.find("th", string="Product Type").find_next_sibling("td").string
     rating_td = book_page_soup.find("th", string="Number of reviews").find_next_sibling("td").string
     image_url_td = book_page_soup.find("img")["src"]
@@ -35,31 +36,34 @@ def scrape_book_data(book_url):
     ratings.append(rating_td)
     image_urls.append(image_url_td)
 
-# Scrape main url page by going into other urls
-base_url = "https://books.toscrape.com/"
-main_url = f"{base_url}catalogue/category/books/poetry_23/index.html"
-main_page = requests.get(main_url)
-main_soup = BeautifulSoup(main_page.content, "html.parser")
-book_links = main_soup.select("h3 a")
+# Function to scrape books from a category page
+def scrape_category(category_url):
+    response = requests.get(category_url)
+    category_soup = BeautifulSoup(response.content, features="html.parser")
+    books = category_soup.select("h3 > a")
+    for book in books:
+        book_url = book.get("href").replace("../../../", "https://books.toscrape.com/catalogue/")
+        scrape_book_data(book_url)
 
-# Correct relative URL to properly scrape the book and print
-for link in book_links:
-    book_url = f"{base_url}{link["href"].replace("../../..", "catalogue") }"
-    scrape_book_data(book_url)
-    print("test")
-# Test to see if list is populated
-print(urls)
+# Scrape categories from main page
+main_url = "https://books.toscrape.com/index.html"
+main_page = requests.get(main_url)
+main_soup = BeautifulSoup(main_page.content, features="html.parser")
+category_links = main_soup.select(".side_categories ul li ul li a")
+
+# Scrape all categories
+for link in category_links:
+    category_url = f"https://books.toscrape.com/{link.get("href")}"
+    scrape_category(category_url)
 
 # CSV headers
-headers = ["URL", "UPC", "title", "price_incl_tax", "price_excl_tax",
-           "quantity_available", "product_description", "category", "rating", "image_url_td"]
+headers = ["URL", "UPC", "Title", "Price_incl_tax", "Price_excl_tax",
+           "Quantity_available", "Product_description", "Category", "Rating", "Image_url"]
 
 # Write to CSV
 with open("books_data.csv", "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(headers)
-
-# Write each book data as row in CSV-
     for i in range(len(urls)):
         row = [urls[i], upcs[i], titles[i], price_incl_taxs[i], price_excl_taxs[i],
            quantity_availables[i], product_descriptions[i], categories[i], ratings[i], image_urls[i]]
